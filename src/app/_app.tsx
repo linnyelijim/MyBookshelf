85b51c060cbc944dda3163110db1d4851aea0b3c
Lindsey Jimenez <lindseyjimenez@gmail.com>
2024-02-19 15:45:35 -0500

    updated file types and GA tag

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -22,3 +22,16 @@ export default function MyApp({ Component, pageProps }: AppProps){
     </>
   );
 } */
+import React from "react";
+import type { AppProps } from "next/app";
+import Layout from "./layout"; // Path to your layout component
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Link from "next/link";
 import Script from "next/script";
 import ScrambleText from "../components/cipher";
