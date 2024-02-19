d5f952e7527155876be8f11f5e6e6390e3b44c68
Lindsey Jimenez <lindseyjimenez@gmail.com>
2024-02-19 15:47:34 -0500

    Corrected file structure.

--- src/app/_app.tsx
+++ src/app/_app.tsx
@@ -1,3 +1,17 @@
+import React from "react";
+import type { AppProps } from "next/app";
+import Layout from "./layout"; // Path to your layout component
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
+
 /* import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { AppProps } from 'next/app';
@@ -22,16 +36,3 @@ export default function MyApp({ Component, pageProps }: AppProps){
     </>
   );
 } */
-import React from "react";
-import type { AppProps } from "next/app";
-import Layout from "./layout"; // Path to your layout component
-
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  );
-}
-
-export default MyApp;
